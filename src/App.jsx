8887c4fce20a26c286a1a29aacfc077cce0f3ff7
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-15 15:06:41 -0300

    feat: adds useEffect to fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import './App.css';
+import { useEffect } from 'react';
 import { useStore } from './store/gameStore';
+import { getSongsFromAlbum } from './services/fetchItunes';
 
 function App() {
-  const { count, inc } = useStore();
+  const { count, inc, songsByAlbum, setSongsByAlbum } = useStore();
+
+  useEffect(() => {
+    let ignore = false;
+    getSongsFromAlbum('1441164816')
+      .then((response) => {
+        if (!ignore) {
+          setSongsByAlbum(response.data.results);
+        }
+      })
+      .catch((error) => {
+        if (!ignore) {
+          console.error(error.message);
+        }
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, [setSongsByAlbum]);
+
+  console.log(songsByAlbum);
 
   return (
     <>
@@ -12,6 +35,7 @@ function App() {
           <span>{count}</span>
           <button onClick={inc}>one up</button>
         </div>
+        <p>{songsByAlbum[0]?.trackName}</p>
       </div>
     </>
   );
--- src/store/gameStore.jsx
+++ src/store/gameStore.jsx
@@ -3,4 +3,13 @@ import { create } from 'zustand';
 export const useStore = create((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
+
+  songsByAlbum: [],
+  // setSongsByAlbum: (songs) => set(() => ({ songsByAlbum: songs })),
+
+  setSongsByAlbum: (songs) =>
+    set(() => {
+      const objFilteredByKind = songs.filter((song) => song.kind === 'song');
+      return { songsByAlbum: objFilteredByKind };
+    }),
 }));
