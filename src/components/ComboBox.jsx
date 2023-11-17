709e1f7c6e1917efa1e2f010437bdd9712d086be
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-17 18:12:30 -0300

    fix: fix <Autocomplete> not resetting its value

--- src/components/ComboBox.jsx
+++ src/components/ComboBox.jsx
@@ -4,7 +4,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 import { useStore } from '../store/GameStore';
 
 export default function ComboBox() {
-  const { nameOfTheSongs, actions } = useStore();
+  const { nameOfTheSongs, answer, actions } = useStore();
   const { saveAnswer } = actions;
 
   return (
@@ -13,16 +13,17 @@ export default function ComboBox() {
       id="combo-box-demo"
       options={nameOfTheSongs}
       // sx={{ width: 300 }}
+      isOptionEqualToValue={(option, value) => option === value || value === ''}
       sx={{ marginBottom: 2 }}
       renderInput={(params) => <TextField {...params} label="Songs" />}
-      // value={}
+      value={answer}
       onChange={(event, newValue) => {
         saveAnswer(newValue);
       }}
-      // inputValue={}
-      onInputChange={(event, newInputValue) => {
-        saveAnswer(newInputValue);
-      }}
+      // inputValue={answer}
+      // onInputChange={(event, newInputValue) => {
+      //   saveAnswer(newInputValue);
+      // }}
     />
   );
 }
