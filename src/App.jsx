9e8185d935998df739282ef7894783e23fe90de3
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-28 20:36:55 -0300

    refactor: creates Game page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,8 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useStore } from './store/gameStore';
-import { getSongsFromAlbum } from './services/fetchItunes';
-import CustomAudioPlayer from './components/CustomAudioPlayer';
-import GuessForm from './components/GuessForm';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import ShowAnswer from './components/ShowAnswer';
-import NextBtn from './components/NextBtn';
-import RestartBtn from './components/RestartBtn';
-import BoxScore from './components/BoxScore';
+import Game from './pages/Game';
 
 function App() {
-  const { songsByAlbum, sortedNumber, actions } = useStore();
-  const { setSongsByAlbum, sortNumber, setNameOfTheSongs } = actions;
-
-  useEffect(() => {
-    let ignore = false;
-    getSongsFromAlbum('1441164816')
-      .then((response) => {
-        if (!ignore) {
-          setSongsByAlbum(response.data.results);
-          setNameOfTheSongs(response.data.results);
-          sortNumber();
-        }
-      })
-      .catch((error) => {
-        if (!ignore) {
-          console.error(error.message);
-        }
-      });
-
-    return () => {
-      ignore = true;
-    };
-  }, [setSongsByAlbum, sortNumber, setNameOfTheSongs]);
-
-  console.log('right answer', songsByAlbum[sortedNumber]?.trackName);
-  console.log('total of tracks', songsByAlbum?.length);
-
-  return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '16px',
-          textAlign: 'center',
-        }}
-      >
-        <BoxScore />
-
-        <ShowAnswer />
-
-        <CustomAudioPlayer />
-
-        <GuessForm />
-
-        <NextBtn />
-
-        <RestartBtn />
-      </Box>
-    </Container>
-  );
+  return <Game />;
 }
 
 export default App;
