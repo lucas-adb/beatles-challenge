f3993ebbf87ffc18fb131f1f7f00ee6cae786848
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-30 18:37:19 -0300

    test: if getSongsFromAlbum has been called

--- src/tests/Game.test.jsx
+++ src/tests/Game.test.jsx
@@ -1,14 +1,19 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render, waitFor } from '@testing-library/react';
+import { describe, expect, test, vi, jest } from 'vitest';
 import Game from '../pages/Game';
-
-// use debug() para checar error
+import * as fetch from '../services/fetchItunes';
 
 describe('Game', () => {
   test("should be able to see 'Beatles Challenge on the screen'", () => {
     const { getByText } = render(<Game />);
 
     expect(getByText('Beatles Guessing Challenge')).toBeInTheDocument();
+  });
+
+  test('if getSongsFromAlbum has been called', async () => {
+    const getSongsFromAlbum = vi.spyOn(fetch, 'getSongsFromAlbum');
+    render(<Game />);
+    expect(getSongsFromAlbum).toHaveBeenCalledOnce();
   });
 });
