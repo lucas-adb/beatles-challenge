54988fbe1ad78b045d8eb066f998e733e436b9af
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-11-30 19:25:22 -0300

    test: mocks getSongsFromAlbum

--- src/tests/Game.test.jsx
+++ src/tests/Game.test.jsx
@@ -1,19 +1,24 @@
 import '@testing-library/jest-dom';
 import { render, waitFor } from '@testing-library/react';
-import { describe, expect, test, vi, jest } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import Game from '../pages/Game';
 import * as fetch from '../services/fetchItunes';
+import { responseMock } from './mocks/responseMock';
 
 describe('Game', () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   test("should be able to see 'Beatles Challenge on the screen'", () => {
     const { getByText } = render(<Game />);
-
     expect(getByText('Beatles Guessing Challenge')).toBeInTheDocument();
   });
 
   test('if getSongsFromAlbum has been called', async () => {
     const getSongsFromAlbum = vi.spyOn(fetch, 'getSongsFromAlbum');
+    getSongsFromAlbum.mockResolvedValue(responseMock);
     render(<Game />);
-    expect(getSongsFromAlbum).toHaveBeenCalledOnce();
+    await waitFor(() => expect(getSongsFromAlbum).toHaveBeenCalledOnce());
   });
 });
