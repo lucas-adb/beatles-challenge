5b12f03db641b05e6fafb7f7c819713dc3f79071
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-12-11 16:44:18 -0300

    refactor: removes unused packages

--- src/services/fetchItunes.js
+++ src/services/fetchItunes.js
@@ -1,19 +1,6 @@
-// import axios from 'axios';
-
 export const AlbumsId = {
   pleasePleaseMe: '1441164816',
 };
-
-// export async function getSongsFromAlbum(id) {
-//   try {
-//     const response = await axios.get(
-//       `https://itunes.apple.com/lookup?id=${id}&entity=song`
-//     );
-//     return response;
-//   } catch (error) {
-//     return error;
-//   }
-// }
 
 export async function getSongsFromAlbum(id) {
   try {
