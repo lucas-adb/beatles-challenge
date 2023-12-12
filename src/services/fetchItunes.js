f0bd6ab8c173d01aca709fbece63d38eb7f9f412
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-12-12 09:26:28 -0300

    fix: fix CORS problem with netlify.toml

--- src/services/fetchItunes.js
+++ src/services/fetchItunes.js
@@ -2,11 +2,25 @@ export const AlbumsId = {
   pleasePleaseMe: '1441164816',
 };
 
+// export async function getSongsFromAlbum(id) {
+//   try {
+//     const response = await fetch(
+//       `https://itunes.apple.com/lookup?id=${id}&entity=song`
+//     );
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     return { data: data };
+//   } catch (error) {
+//     console.error(error);
+//     return error;
+//   }
+// }
+
 export async function getSongsFromAlbum(id) {
   try {
-    const response = await fetch(
-      `https://itunes.apple.com/lookup?id=${id}&entity=song`
-    );
+    const response = await fetch(`/api/lookup?id=${id}&entity=song`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
